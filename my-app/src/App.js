1d71e36affb5926c4327360e5f4c6c26530cd14d
Devon <ddarrowphl@gmail.com>
2022-08-28 15:20:49 -0400

    started styling implementation

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-// API Base url
+// API Base url (no auth required)
 const BASE_URL = 'https://countriesnow.space/api/v0.1/countries'
 
 // Configs
@@ -19,18 +19,18 @@ const cityConfig = {
     'Venice': 'Venice, the capital of northern Italy\'s Veneto region, is built on more than 100 small islands in a lagoon in the Adriatic Sea. It has no roads, just canals.'
   },
   DE: {
-    'Berlin': 'Berlin, Germany\'s capital, dates to the 13th century. Divided during the Cold War, its 18th-century Brandenburg Gate has become a symbol of reunification. The city\'s also known for its art scene and modern landmarks like the gold-colored, swoop-roofed Berliner Philharmonie, built in 1963.'
+    'Berlin': 'Berlin, Germany\'s capital, dates to the 13th century. The city\'s also known for its art scene and modern landmarks like the gold-colored, swoop-roofed Berliner Philharmonie, built in 1963.'
   },
   ES: {
-    'Barcelona': 'Barcelona, the cosmopolitan capital of Spain\'s Catalonia region, is known for its art and architecture. The fantastical Sagrada Família church and other modernist landmarks designed by Antoni Gaudí dot the city. Museu Picasso and Fundació Joan Miró feature modern art by their namesakes. City history museum MUHBA, includes several Roman archaeological sites.'
+    'Barcelona': 'Barcelona, the cosmopolitan capital of Spain\'s Catalonia region, is known for its art and architecture. The fantastical Sagrada Família church and other modernist landmarks designed by Antoni Gaudí dot the city.'
   },
   FR: {
-    'Paris': 'Paris, France\'s capital, is a major European city and a global center for art, fashion, gastronomy and culture. Its 19th-century cityscape is crisscrossed by wide boulevards and the River Seine. Beyond such landmarks as the Eiffel Tower and the 12th-century, Gothic Notre-Dame cathedral, the city is known for its cafe culture and designer boutiques along the Rue du Faubourg Saint-Honoré.'
+    'Paris': 'Paris, France\'s capital, is a major European city and a global center for art, fashion, gastronomy and culture. Its 19th-century cityscape is crisscrossed by wide boulevards and the River Seine.'
   },
-  NL: {'Amsterdam': 'Amsterdam is the Netherlands\' capital, known for its artistic heritage, elaborate canal system and narrow houses with gabled facades, legacies of the city\'s 17th-century Golden Age. Its Museum District houses the Van Gogh Museum, works by Rembrandt and Vermeer at the Rijksmuseum, and modern art at the Stedelijk. Cycling is key to the city\'s character, and there are numerous bike paths.'
+  NL: {'Amsterdam': 'Amsterdam is the Netherlands\' capital, known for its artistic heritage, elaborate canal system and narrow houses with gabled facades, legacies of the city\'s 17th-century Golden Age.'
   },
   GB: {
-    'London': 'London, the capital of England and the United Kingdom, is a 21st-century city with history stretching back to Roman times. At its centre stand the imposing Houses of Parliament, the iconic \'Big Ben\' clock tower and Westminster Abbey, site of British monarch coronations. Across the Thames River, the London Eye observation wheel provides panoramic views of the South Bank cultural complex, and the entire city.'
+    'London': 'London, the capital of England and the United Kingdom, is a 21st-century city with history stretching back to Roman times.'
   }
 }
 
@@ -70,20 +70,26 @@ const CountryCardGrid = () => {
         //iterate through each card and display the appropriate data
         //display height and width of card differently depending on which child it is - i.e. if first or last, 
       <>
-        <div>
+        {/* <div className='card-children-wrapper'> */}
         { 
-          countries.map(country => {
+          countries.map((country, index) => {
             return (
             <>
-              <div>{country.country}</div>
-              <div>{Object.keys(cityConfig[country.iso2]).join('')}</div>
-              <div>{Object.values(cityConfig[country.iso2]).join('')}</div>
-              <button>Explore More</button>
+              <div className='card-wrapper' data-index={index}>
+                <div className='card-country-name'>{country.country}</div>
+                <div className='card-city-name'>{Object.keys(cityConfig[country.iso2]).join('')}</div>
+                <div className="card-overlay">
+                  <div className='card-country-name'>{country.country}</div>
+                  <div className='card-city-name'>{Object.keys(cityConfig[country.iso2]).join('')}</div>
+                  <div className='card-desc'>{Object.values(cityConfig[country.iso2]).join('')}</div>
+                  <button className='card-explore-more'>Explore More</button>
+                </div>
+              </div>
             </>
             )
           }) 
         }
-        </div>
+        {/* </div> */}
       </>
      )
     } else {
@@ -95,12 +101,16 @@ const CountryCardGrid = () => {
 const App = () => {
   return (
     <>
-      <div className=''>
-        <div>FRONT-END</div>
-        <h1 className="header-card">valtech_</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    <div className='card-parent'>
+      <div className='card-grid-wrapper'>
+        <div className='card-main-wrapper'>
+          <div>FRONT-END</div>
+          <h1 className="header-card">valtech_</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+        <CountryCardGrid/>
       </div>
-      <CountryCardGrid/>
+      </div>
     </>
   );
 }
